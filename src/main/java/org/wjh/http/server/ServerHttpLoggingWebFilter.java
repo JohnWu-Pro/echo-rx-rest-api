0cb6de1446e34f9d92f07da75ef140002ba93997
wujuhua <wujuhua@ejfun.com>
2020-02-28 22:48:07 -0500

    Simplify ServerHttpLoggingWebFilter

--- src/main/java/org/wjh/http/server/ServerHttpLoggingWebFilter.java
+++ src/main/java/org/wjh/http/server/ServerHttpLoggingWebFilter.java
@@ -1,40 +1,45 @@
 package org.wjh.http.server;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import org.wjh.http.server.ServerHttpLoggingHandler.LoggingServerHttpRequest;
 
-import brave.Span;
+import brave.Tracer;
 import reactor.core.publisher.Mono;
 
-class ServerHttpLoggingWebFilter implements WebFilter, Ordered {
+@Component
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+@ConditionalOnProperty(value = "server.http.logging", havingValue = "true", matchIfMissing = false)
+public class ServerHttpLoggingWebFilter implements WebFilter, Ordered {
 
-    private static final String TRACE_REQUEST_ATTR = "org.springframework.cloud.sleuth.instrument.web.TraceWebFilter.TRACE";
+    private final Tracer tracer;
+
+    private ServerHttpLoggingWebFilter(Tracer tracer) {
+        this.tracer = tracer;
+    }
 
     @Override
     public int getOrder() {
         // On the way in, the filter should be after the content decryption filter (if any), and prior to spring.security.filter (order=-100)
-        // On the way out, the filter should be (almost) the last one
+        // On the way out, the filter is not applicable
         return -1000;
     }
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        triggerRequestLoggingIfApplicable(exchange);
-        return chain.filter(exchange);
+        return chain.filter(exchange).doOnSubscribe(s -> triggerRequestLoggingIfApplicable(exchange));
     }
 
     private void triggerRequestLoggingIfApplicable(ServerWebExchange exchange) {
         if (exchange.getRequest() instanceof LoggingServerHttpRequest) {
             LoggingServerHttpRequest decorated = (LoggingServerHttpRequest) exchange.getRequest();
-            decorated.triggerLogging(currentSpan(exchange));
+            decorated.triggerLogging(tracer.currentSpan());
         }
-    }
-
-    private Span currentSpan(ServerWebExchange exchange) {
-        return (Span) exchange.getAttributes().get(TRACE_REQUEST_ATTR);
     }
 
 }
