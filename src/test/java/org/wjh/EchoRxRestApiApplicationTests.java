73b3af53d9b8232f45378766a7108ce94fa05520
wujuhua <wujuhua@ejfun.com>
2020-07-05 21:29:30 -0400

    Rename test class

--- src/test/java/org/wjh/EchoRxRestApiApplicationTests.java
+++ src/test/java/org/wjh/EchoRxRestApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class EchoRestApiApplicationTests {
+public class EchoRxRestApiApplicationTests {
 
     @Test
     void contextLoads() {
